d5b32eb5b0630b4ee295e3cdb082c4cb4d18211e
kehowell92 <138188533+kehowell92@users.noreply.github.com>
2024-10-24 22:11:25 -0500

    SettingsMenu functional, cancel button to send back to MainActivity

--- app/src/main/java/com/example/dreambound/SettingsMenu.java
+++ app/src/main/java/com/example/dreambound/SettingsMenu.java
@@ -1,24 +1,72 @@
 package com.example.dreambound;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsMenu extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
+    Button logOut;
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.settings_menu);
+
+        //initializing Firebase
+        fAuth = FirebaseAuth.getInstance();
+
+        //accessing LogOut button from XML && creating click listener
+        logOut = findViewById(R.id.logout_btn);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogOut();
+            }
+        });
+
+        //accessing Cancel button from XML && creating click listener
+        cancel = findViewById(R.id.Cancel_btn);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               Intent intent = new Intent(SettingsMenu.this, MainActivity.class);
+               startActivity(intent);
+               finish();
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    //creating LogOut method
+    private void LogOut()
+    {
+        fAuth.signOut();
+        Intent intent = new Intent(SettingsMenu.this, LogIn.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
