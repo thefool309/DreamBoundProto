e615f0ec7d0b3d00496d6915bb8beeb87897afea
thefool309 <themagician309@gmail.com>
2024-10-25 20:20:50 -0500

    Working post merge

--- app/src/main/java/com/example/dreambound/GameEngine.java
+++ app/src/main/java/com/example/dreambound/GameEngine.java
@@ -76,15 +76,5 @@ public class GameEngine {
             }
         }
     }
-    private void startEngineAndPullData(){
-        if (gameEngine == null) {
-            gameEngine = new GameEngine(getContext());
-            creatures = gameEngine.getCreaturesLoadedIn();
-            staticObjects = gameEngine.getStaticObjects();
-            allObjects = gameEngine.getAllObjects();
-            obstacles = gameEngine.getObstacles();
-            collidables = gameEngine.getCollisionObjects();
-            player = gameEngine.getPlayer();
-        }
-    }
+
 }
